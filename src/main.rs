21b67873fe7d1604d929a8cf41ba8d0abdb467a2
maxwase <max.vvase@gmail.com>
2024-03-29 01:17:24 +0200

    Version bump

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,9 @@ use inverse_kinematics::KinematicsApp;
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> Result<(), eframe::Error> {
-    let native_options = eframe::NativeOptions {
-        maximized: true,
-        ..Default::default()
-    };
     eframe::run_native(
         "Inverse kinematics",
-        native_options,
+        Default::default(),
         Box::new(|_| Box::<KinematicsApp>::default()),
     )
 }
@@ -16,21 +12,18 @@ fn main() -> Result<(), eframe::Error> {
 // when compiling to web using trunk.
 #[cfg(target_arch = "wasm32")]
 fn main() {
-    // Make sure panics are logged using `console.error`.
-    console_error_panic_hook::set_once();
-
-    // Redirect tracing to console.log and friends:
-    tracing_wasm::set_as_global_default();
+    const CANVAS_ID: &str = "inverse_kinematics";
 
     let web_options = eframe::WebOptions::default();
 
     wasm_bindgen_futures::spawn_local(async {
-        eframe::start_web(
-            "inverse_kinematics", // hardcode it
-            web_options,
-            Box::new(|_| Box::<KinematicsApp>::default()),
-        )
-        .await
-        .expect("start kinematics app");
+        eframe::WebRunner::new()
+            .start(
+                CANVAS_ID,
+                web_options,
+                Box::new(|_| Box::<KinematicsApp>::default()),
+            )
+            .await
+            .expect("start kinematics app");
     });
 }
